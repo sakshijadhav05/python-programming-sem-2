45dc7b9180c03c8214f2daeca7f6a1a1f9df0401
sakshijadhav05 <sakshi001jadhav@gmail.com>
2026-02-17 13:19:37 +0530

    Add files via upload

---
+++ exp1.py
@@ -0,0 +1,13 @@
+# program of row
+"""
+Created on Tue Feb 17 07:13:15 2026
+
+@author: sakshi jadhav 
+"""
+
+n=int(input("enter number of rows:"))
+
+for i in range(1,n+1):
+    for j in range(1,i+1):
+     print(j,end="")
+     print()
