a6612431b0ced1b56c4e5b80f69dacfc90b8596c
sakshijadhav05 <sakshi001jadhav@gmail.com>
2026-02-17 13:18:55 +0530

    Add files via upload

---
+++ exp2.2.py
@@ -0,0 +1,16 @@
+num1 =int(input("Enter First Number:"))
+num2 =int(input("Enter Second Number:"))
+
+sum=num1+num2
+print("Addition=",sum)
+
+
+sub=num1-num2
+print("substract=",sub)
+
+multiply=num1*num2
+print("multiplycation=",multiply)
+
+divide=num1%num2
+print("division=",divide)
+
