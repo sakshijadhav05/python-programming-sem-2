0aea1771cacd905cc39d668c9a51b15f28ae7ab5
sakshijadhav05 <sakshi001jadhav@gmail.com>
2026-03-17 13:23:09 +0530

    Add files via upload

---
+++ exp5act6.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 07:37:58 2026
+
+@author: sakshi jadhav 
+"""
+
+#  Count frequency of items purchased
+
+items = ["apple", "banana", "apple", "orange", "banana", "apple"]
+
+frequency = {}
+
+for item in items:
+    if item in frequency:
+        frequency[item] += 1
+    else:
+        frequency[item] = 1
+
+print(frequency)
