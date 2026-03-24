526d30914a79fb5e10016e31ee2f4a9a8e6b5aaf
sakshijadhav05 <sakshi001jadhav@gmail.com>
2026-03-24 13:07:43 +0530

    Add files via upload

---
+++ exp6act1.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 12 11:05:54 2026
+
+@author:sakshi jadhav 
+"""
+# Store expenses
+file = open("expenses.txt", "a")
+
+n = int(input("Enter number of days: "))
+for i in range(n):
+    amt = float(input(f"Enter expense for day {i+1}: "))
+    file.write(str(amt) + "\n")
+
+file.close()
+
+# Calculate total
+file = open("expenses.txt", "r")
+total = 0
+
+for line in file:
+    total += float(line.strip())
+
+file.close()
+
+print("Total Monthly Expense =", total)
