b6e932e0b30411b0787db5e7fabbc229dedd9b79
sakshijadhav05 <sakshi001jadhav@gmail.com>
2026-04-29 14:19:04 +0530

    Add files via upload

---
+++ exp4prg4.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 14:12:40 2026
+
+@author: User
+"""
+
+feedback = input("Enter customer feedback: ")
+
+# Reverse the string
+reversed_feedback = feedback[::-1]
+
+print("\n--- Result ---")
+print("Original Feedback:", feedback)
+print("Reversed Feedback:", reversed_feedback)
