da04b69901e9b934fc4c60815ac7802822fcbb1d
sakshijadhav05 <sakshi001jadhav@gmail.com>
2026-04-29 13:45:21 +0530

    Add files via upload

---
+++ exp3prg4.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 13:16:45 2026
+
+@author: User
+"""
+
+# Traffic Police Speed Check
+
+# Movie Ticket Eligibility Check
+
+# Loan Approval System
+
+# Receipt Copy Printer using Nested Loops
+
+# Multiplication tables from 1 to 10
+
+n = int(input("Enter number of rows: ")) 
+for i in range(1, n + 1):
+   for j in range(1, i + 1): 
+        print(j, end=" ")
+        print()
