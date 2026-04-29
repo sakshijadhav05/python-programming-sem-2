62bd1d210c8b369bc1a022c1b5eb307ca43fb33c
sakshijadhav05 <sakshi001jadhav@gmail.com>
2026-04-29 13:20:27 +0530

    Add files via upload

---
+++ exp2prg1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 13:16:45 2026
+
+@author: User
+"""
+
+year = int(input("Enter year: "))
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0): 
+  print("Leap Year")
+else:
+  print("Not a Leap Year")
