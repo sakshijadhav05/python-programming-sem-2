1683f269a69db1be1fad7e109dac1fae6895ff02
sakshijadhav05 <sakshi001jadhav@gmail.com>
2026-04-29 13:28:45 +0530

    Add files via upload

---
+++ exp2prg4.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 13:16:45 2026
+
+@author: User
+"""
+
+# Traffic Police Speed Check
+
+# Movie Ticket Eligibility Check
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to watch the movie.")
+else:
+    print("You are not eligible. Age must be 18 or above.")
