b8be0e5de1c39c0224e7a697741461b64d7dd10e
sakshijadhav05 <sakshi001jadhav@gmail.com>
2026-04-29 13:41:27 +0530

    Add files via upload

---
+++ exp3prg2.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 13:16:45 2026
+
+@author: User
+"""
+
+# Traffic Police Speed Check
+
+# Movie Ticket Eligibility Check
+
+# Loan Approval System
+
+# Receipt Copy Printer using Nested Loops
+
+# Multiplication tables from 1 to 10
+
+for i in range(1, 11):          # Table number
+    print(f"\nMultiplication Table of {i}")
+    print("-" * 25)
+    
+    for j in range(1, 11):      # Multiply from 1 to 10
+        print(f"{i} x {j} = {i * j}")
