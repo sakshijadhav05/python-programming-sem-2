2203a290712bfecb5409926f4e49f35d1f5968b4
sakshijadhav05 <sakshi001jadhav@gmail.com>
2026-04-29 13:30:39 +0530

    Add files via upload

---
+++ exp2prg5.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 13:16:45 2026
+
+@author: User
+"""
+
+# Traffic Police Speed Check
+
+# Movie Ticket Eligibility Check
+
+# Loan Approval System
+
+salary = float(input("Enter your monthly salary: "))
+
+if salary >= 25000:
+    print("Loan Approved")
+else:
+    print("Loan Rejected")
